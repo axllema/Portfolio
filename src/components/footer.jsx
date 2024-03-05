e7b1ef402bc36a0b0c746801ae6e70ef1fe8af70
axllema <axellexm@gmail.com>
2024-03-05 13:04:31 +0100

    footer all rights reserved added + translation working

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -3,15 +3,23 @@
 // mettre navicons footer , pour git & linkedin qd meme
 import Logo from '../components/logo';
 import FooterIcons from '../components/footericons';
+import { useContext} from 'react';
+import { LanguageContext } from '../pages/Homepage';
+import FooterTranslations from '../translation/FooterTranslations.jsx';
 import '../scss/layout/_footer.scss';
 import '../scss/style.scss';
 
 function Footer() {
+
+    const { language } = useContext(LanguageContext);
+    const translations = FooterTranslations[language];
+
     return (
         <nav className="footer">
             <div className="footer-items">
                 <FooterIcons />
                 <Logo className="footer-items-logo" imageClassName="footer-items-logo-image" />
+                <p className="footer-items-text"> <span className="footer-items-text-span"> © </span> {translations.footertext || "Axelle Maolé - Tous droits réservés - 2024."}</p>
             </div>
         </nav>
     );
---
+++ src/translation/FooterTranslations.jsx
@@ -0,0 +1,12 @@
+const FooterTranslations = {
+    fr: {
+        footertext: "Axelle Maolé - Tous droits réservés - 2024."
+        // Ajoutez d'autres traductions françaises ici si nécessaire
+    },
+    en: {
+        footertext: "Axelle Maolé - All rights reserved - 2024."
+        // Ajoutez d'autres traductions anglaises ici si nécessaire
+    }
+};
+
+export default FooterTranslations;
