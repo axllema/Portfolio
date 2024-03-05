c65f33b02ee55bcf2c482dbeae115f9abcde4a40
axllema <axellexm@gmail.com>
2024-03-05 02:17:44 +0100

    changes in skills jsx & scss, to have better design for logo components

--- src/components/skills.jsx
+++ src/components/skills.jsx
@@ -20,6 +20,23 @@ import '../scss/style.scss';
 function Skills() {
     const { language } = useContext(LanguageContext);
 
+    const developerSkills = [
+        { icon: HTML, name: "HTML 5" },
+        { icon: CSS, name: "CSS 3" },
+        { icon: JAVASCRIPT, name: "JavaScript" },
+        { icon: React, name: "React" },
+        { icon: SASS, name: "SASS" },
+        { icon: Redux, name: "Redux" },
+        { icon: Github, name: "Github" }
+    ];
+
+    const designerSkills = [
+        { icon: Photoshop, name: "Photoshop" },
+        { icon: Illustrator, name: "Illustrator" },
+        { icon: Indesign, name: "Indesign" },
+        { icon: XD, name: "Adobe XD" }
+    ];
+
     return (
         <section className="skills" id="skills">
         <div className="skills-container">
@@ -27,56 +44,35 @@ function Skills() {
                 <h2 className="skills-title-text">{SkillsTranslations[language].skills.title || "Compétences"}</h2>
             </div>
             <div className="skills-list-container">
-                <ul className="skills-list">
-                    <li className="skills-list-item">
-                        <img src={HTML}  className="skills-list-item-icon" alt="HTML5 icon" />
-                        <p className="skills-list-item-name">HTML 5</p>
-                    </li>
-                    <li className="skills-list-item">
-                        <img src={CSS}  className="skills-list-item-icon" alt="CSS3 icon" />
-                        <p className="skills-list-item-name">CSS 3</p>
-                    </li>
-                    <li className="skills-list-item">
-                        <img src={JAVASCRIPT}  className="skills-list-item-icon" alt="Javascript icon" />
-                        <p className="skills-list-item-name">JavaScript</p>
-                    </li>
-                    <li className="skills-list-item">
-                        <img src={React}  className="skills-list-item-icon" alt="React icon" />
-                        <p className="skills-list-item-name">React</p>
-                    </li>
-                    <li className="skills-list-item">
-                        <img src={SASS}  className="skills-list-item-icon" alt="SASS icon" />
-                        <p className="skills-list-item-name">SASS</p>
-                    </li>
-                    <li className="skills-list-item">
-                        <img src={Redux}  className="skills-list-item-icon" alt="Redux icon" />
-                        <p className="skills-list-item-name">Redux</p>
-                    </li>
-                    <li className="skills-list-item">
-                        <img src={Github}  className="skills-list-item-icon" alt="Github icon" />
-                        <p className="skills-list-item-name">Github</p>
-                    </li>
-                    <li className="skills-list-item">
-                        <img src={Photoshop}  className="skills-list-item-icon" alt="Photoshop icon" />
-                        <p className="skills-list-item-name">Photoshop</p>
-                    </li>
-                    <li className="skills-list-item">
-                        <img src={Illustrator}  className="skills-list-item-icon" alt="Illustrator icon" />
-                        <p className="skills-list-item-name">Illustrator</p>
-                    </li>
-                    <li className="skills-list-item">
-                        <img src={Indesign}  className="skills-list-item-icon" alt="Indesign icon" />
-                        <p className="skills-list-item-name">Indesign</p>
-                    </li>
-                    <li className="skills-list-item">
-                        <img src={XD}  className="skills-list-item-icon" alt="XD icon" />
-                        <p className="skills-list-item-name">Adobe XD</p>
-                    </li>
-                </ul>
+
+                <div className="skills-list">
+                            <ul className="skills-list-dev">
+                                {developerSkills.map((skill, index) => (
+                                    <li className="skills-list-item" key={index}>
+                                        <img src={skill.icon} className="skills-list-item-icon"
+                                        alt={skill.name} loading="lazy"/>
+                                        <p className="skills-list-item-name">{skill.name}</p>
+                                    </li>
+                                ))}
+                            </ul>
+
+                        <div className="skills-list">
+                            <ul className="skills-list-design">
+                                {designerSkills.map((skill, index) => (
+                                    <li className="skills-list-item" key={index}>
+                                        <img src={skill.icon} className="skills-list-item-icon"
+                                        alt={skill.name} loading="lazy" />
+                                        <p className="skills-list-item-name">{skill.name}</p>
+                                    </li>
+                                ))}
+                            </ul>
+                        </div>
+
+                    </div>
+                </div>
             </div>
-        </div>
-    </section>
-);
+        </section>
+    );
 }
 
 export default Skills;
